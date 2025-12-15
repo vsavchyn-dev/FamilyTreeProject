cbfc72a1024ac41908a4c66b507a689c9cefa6bb
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2025-12-14 19:40:13 -0500

    init repo for project

---
+++ FamilyTreeBackend/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("org.springframework.boot").version("3.2.1")
+    id("io.spring.dependency-management").version("1.1.4")
+}
+
+group = "org.vnn"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ FamilyTreeBackend/src/main/java/FamilyTree/FamilyTreeBackend.java
@@ -0,0 +1,11 @@
+package FamilyTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeBackend {
+    public static void main(String... args) {
+        SpringApplication.run(FamilyTreeBackend.class, args);
+    }
+}
